24e4715a2c1cfade2d1fd4502c54cefc709d4643
Shin <shinnosuke.satoh@pparc.gp.tohoku.ac.jp>
2022-12-13 16:09:52 +0900

    Module testings

---
+++ mod_main.py
@@ -0,0 +1,18 @@
+""" mod_main.py
+
+Created on Dec 13 2022
+@author: Shin Satoh
+
+Description:
+
+"""
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+from mod_test import convert0
+
+A = np.array([0, 1, 2])
+B = convert0(A)
+print(B)
---
+++ mod_test.py
@@ -0,0 +1,22 @@
+""" mod_test.py
+
+Created on Dec 13 2022
+@author: Shin Satoh
+
+Description:
+
+"""
+
+import numpy as np
+
+
+def convert0(A):
+    """ A will be converted to B.
+
+    Returns:
+        ndarray: B
+    """
+
+    B = A*np.pi
+
+    return B
